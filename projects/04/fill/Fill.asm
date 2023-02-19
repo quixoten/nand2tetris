72e587f50e968d2e9ccbad640a0e9070dc12d039
Devin Christensen <quixoten@gmail.com>
2023-02-19 14:44:44 -0700

    implement fill.asm

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,60 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+// last-word = SCREEN + 8192 (8K)
+  @SCREEN
+  D=A
+
+  @8192
+  D=D+A
+
+  @last-word
+  M=D
+
+(FILL-ENTIRE-SCREEN)
+  // start <next-word> at the top left of the screen.
+  // <next-word> is a pointer
+    @SCREEN
+    D=A
+
+    @next-word
+    M=D
+
+  // set the fill-value based on KBD state
+    @fill-value
+    M=0
+
+    @KBD
+    D=M
+
+    @KEY-PRESSED
+    D;JNE
+
+    @FILL-NEXT-WORD
+    0;JMP
+
+    (KEY-PRESSED)
+    @fill-value
+    M=-1
+
+  (FILL-NEXT-WORD)
+
+    // fill in the next 16 pixels
+      @fill-value
+      D=M
+      @next-word
+      A=M
+      M=D
+
+    // advance the <next-word> forward
+      @next-word
+      MD=M+1
+
+    @last-word
+    D=M-D // D = last-word - next-word
+
+    @FILL-NEXT-WORD
+    D;JGT // last-word - next-word > 0, fill next word
+
+@FILL-ENTIRE-SCREEN
+0;JMP
