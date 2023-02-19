e9f7a8a1403c8ad9cfa37d057084bde6101b287f
Devin Christensen <quixoten@gmail.com>
2023-02-19 15:48:49 -0700

    implement Mult.asm

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -9,4 +9,66 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+// RAM[2] = 0
+  @0
+  D=A
+  @R2
+  M=D
+
+// operand = RAM[0]
+  @R0
+  D=M
+
+  @END-OF-PROGRAM
+  D;JEQ // if RAM[0] = 0, we're done
+
+  @operand
+  M=D
+
+// iterations = RAM[1]
+  @R1
+  D=M
+
+  @END-OF-PROGRAM
+  D;JEQ // if RAM[1] = 0, we're done
+
+  @iterations
+  M=D
+
+// if R0 < R1, swap <iterations> and <operand> so we finish in fewer clock
+// cycles
+
+  @R0
+  D=M-D // D = R0 - R1
+
+  @ITERATE
+  D;JGE // R0 >= R1, start iterating
+
+  // operand = RAM[1]
+    @R1
+    D=M
+    @operand
+    M=D
+
+  // iterations = RAM[0]
+    @R0
+    D=M
+    @iterations
+    M=D
+
+(ITERATE)
+  @operand
+  D=M
+
+  @R2
+  M=D+M
+
+  @iterations
+  MD=M-1
+
+  @ITERATE
+  D;JGT
+
+(END-OF-PROGRAM)
+  @END-OF-PROGRAM
+  0;JMP
--- scripts/test.sh
+++ scripts/test.sh
@@ -4,16 +4,24 @@ root_dir="$(readlink -f "$(dirname "$(readlink -f "$0")")/..")"
 projects_dir="${root_dir}/projects"
 tools_dir="${root_dir}/tools"
 hardware_simulator="${tools_dir}/HardwareSimulator.sh"
+cpu_emulator="${tools_dir}/CPUEmulator.sh"
 
-for project_number in 01 02 03; do
-	while read -rs hdl; do
-		test_output=$(bash "$hardware_simulator" "${hdl%%.hdl}.tst" 2>&1)
+
+for project_number in 01 02 03 04; do
+	while read -rs tst; do
+		if [[ $project_number -eq 4 ]]; then
+			test_tool="${cpu_emulator}"
+		else
+			test_tool="${hardware_simulator}"
+		fi
+
+		test_output=$(bash "$test_tool" "${tst}" 2>&1)
 
 		if [[ $? -eq 0 ]]; then
-			echo -e "\e[32m✓ projects/${hdl##*projects/}\e[0m"
+			echo -e "\e[32m✓ projects/${tst##*projects/}\e[0m"
 		else
-			printf "\e[31m%-30s\e[0m" "✗ projects/${hdl##*projects/}"
+			printf "\e[31m%-30s\e[0m" "✗ projects/${tst##*projects/}"
 			echo "${test_output}" | sed 's/^/    /'
 		fi
-	done < <(find "${projects_dir}/${project_number}" -name "*.hdl")
+	done < <(find "${projects_dir}/${project_number}" -name "*.tst")
 done
